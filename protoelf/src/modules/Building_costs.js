160a4b760bb69b9d5f2cd0a28999a53802ae7456
lampinep <pauli.lampinen@tuni.fi>
2020-01-25 17:08:03 +0200

    Building costs + mining fix

---
+++ protoelf/src/modules/Building_costs.js
@@ -0,0 +1,26 @@
+import { Double } from "mongodb";
+
+/* Module functions
+
+
+
+
+
+
+*/
+// Building costs stored in a map
+var building_equations = [
+    {name: "b1", eq: function formula(n) { return 2*level}},
+    {name: "b2", eq: "3 * level + ( level + level ) / 3"}
+];
+
+
+export function getBuildingCost(name_id, colony_id){
+    var level = 5// migi, haetaan databasesta colony_idllä
+    var equation = building_equations.map(name_id); // Voiko mapissa olla 3 
+    return equation(level);
+    console.log(equation)
+    
+    
+}
+
