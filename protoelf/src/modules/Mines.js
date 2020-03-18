78f660ff6e5457081f1e3581d74378b5b2cbfc9a
lampinep <pauli.lampinen@tuni.fi>
2020-03-18 18:45:56 +0200

    Added a helper function

--- protoelf/src/modules/Mines.js
+++ protoelf/src/modules/Mines.js
@@ -3,10 +3,24 @@ const Bds = require('./Buildings.js');
 
 */
 
+/**
+ * 
+ * @param {*} building_id Id of the building
+ * @param {*} building_level Level of the building
+ * @returns Resource generation rate per one second
+ */
 function getResourcesPerSecond(building_id, building_level){
     let c = parseInt(building_level);
     return Bds.buildingEquations[building_id].production_eq(c);
 };
 
-console.log(Bds.buildingEquations)
-console.log(getResourcesPerSecond("Mine_res_1", 5));
+function getResourcesPerSecondAll(level1,level2,level3){
+    let a = parseInt(level1);
+    let b = parseInt(level2);
+    let c = parseInt(level3);
+    return [Bds.buildingEquations['Mine_res_1'].production_eq(a),
+            Bds.buildingEquations['Mine_res_2'].production_eq(b),
+            Bds.buildingEquations['Mine_res_3'].production_eq(c)];
+};
+
+console.log(getResourcesPerSecondAll(1,1,1))
