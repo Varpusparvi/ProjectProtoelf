748b71bc849fc83053fbc5572ac11aa4c5477ba6
lampinep <pauli.lampinen@tuni.fi>
2020-03-22 16:31:35 +0200

    Updated Move Functions

--- protoelf/src/modules/MoveFunctions.js
+++ protoelf/src/modules/MoveFunctions.js
@@ -15,25 +15,53 @@ export function getMoveTime(xy_from, xy_to, speed){
 /**
  * 
  * @param {*} event Event object from the database/server
- * @returns [[res1,res2,res3], to_as_ColId]
+ * @returns If phase === start : [new_phase,[res1,res2,res3],[force:number_of_the_force],...]
+ * @returns If phase === arrival : [new_phase, resources_carried_by_fleet as [res1,res2,res3], resource to be delivered as [res1,res2,res3], resources to delivered to as ColID, [force:number_of_the_force],...]
+ * @returns If phase === end : [resources_carried_by_fleet as [res1,res2,res3], added troops to as ColID, [force:number_of_the_force],...]
+ * @returns Error if problems with event.phase
  */
 export function transport(event){
     let resources = [event.resources[0],event.resources[1],event.resources[2]];
     let to = event.to;
-    return [resources, to];
+    if(event.phase === "start"){
+        return ["arrival",resources, gatherForces(event)];
+    }
+    if(event.phase === "arrival"){
+        return ["end", [0,0,0], resources, to, gatherForces(event)];
+    }
+    if(event.phase === "end"){
+        return [[0,0,0], event.from, gatherForces(event)];
+    }
+    return "Error with event.phase";
 };
 
 /**
  * 
  * @param {*} event Event object from the database/server
- * @returns [[res1,res2,res3], to_as_colID, ["ship1",5 as how many ship1],...,["shipN",3]]
+ * @returns If phase === start : [new_phase,[res1,res2,res3], troops to be removed from as ColID, ["ship1",5 as how many ship1],...,["shipN",3]]
+ * @returns If phase === arrival : [[res1,res2,res3], to_as_colID, ["ship1",5 as how many ship1],...,["shipN",3]]
+ * @returns Error with event.phase
  */
 export function deploy(event){
     let resources = [event.resources[0],event.resources[1],event.resources[2]];
     let to = event.to;
-    let return_arr = [resources, to];
+    
+    if(event.phase === "start"){
+        return ["arrival",resources, event.from, gatherForces(event)];
+    }
+    if(event.phase === "arrival"){
+        return [resources, event.to, gatherForces];
+    }
+    return "Error with event.phase";
+};
+
+
+
+
+function gatherForces(event){
+    let return_arr = [];
     for(let item in event.forces){
         return_arr.push([item,event.forces[item]]);
     }
     return return_arr;
-};
+}
